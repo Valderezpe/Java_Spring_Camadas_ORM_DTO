6ccb762a2e2089c5b798db5561753172c6e46a4f
Jose Valderez <valderezpe@gmail.com>
2021-12-07 08:13:20 -0300

    Criando camadas, JpaRepository, Banco-H2

---
+++ src/main/java/com/example/demo/CamadasJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamadasJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamadasJavaSpringApplication.class, args);
+	}
+
+}
